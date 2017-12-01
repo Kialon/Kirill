12b2b891501277581a70a641e1b8b78e11553862
Kialon <33956106+Kialon@users.noreply.github.com>
2017-12-01 14:50:19 +0200

    Add files via upload

--- MainActivity.cs
+++ MainActivity.cs
@@ -5,15 +5,45 @@ using Android.OS;
 namespace App2
 {
     [Activity(Label = "App2", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : Activity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-            SetContentView(Resource.Layout.Main);
+        private ViewFlipper _flipper;
+        private Button btnPrev, btnNext;
+            protected override void OnCreate(Bundle savedInstanceState)
+            {
+                  base.OnCreate(savedInstanceState);
 
+                  // Set our view from the "main" layout resource
+                  SetContentView(Resource.Layout.Main);
+            _flipper = FindViewById<ViewFlipper>(Resource.Id.viewFlipper);
+
+            btnNext = FindViewById<Button>(Resource.Id.btnNext);
+            btnPrev = FindViewById<Button>(Resource.Id.btnPrev);
+
+            // Use button clicks to cycle through views
+            btnNext.Click += (sender, e) => {
+                // Use custom animations
+                _flipper.SetOutAnimation(this, Resource.Animation.slideout_left);
+                _flipper.SetInAnimation(this, Resource.Animation.slidein_right);
+                // Use Android built-in animations
+                //_flipper.SetInAnimation(this, Android.Resource.Animation.SlideInLeft);      
+                //_flipper.SetOutAnimation(this, Android.Resource.Animation.SlideOutRight);
+                _flipper.ShowNext();
+            };
+            btnPrev.Click += (sender, e) => {
+                // Use custom animations
+                _flipper.SetOutAnimation(this, Resource.Animation.slideout_right);
+                _flipper.SetInAnimation(this, Resource.Animation.slidein_left);
+                // Use Android built-in animations
+                //_flipper.SetInAnimation(this, Android.Resource.Animation.SlideInLeft);
+                //_flipper.SetOutAnimation(this, Android.Resource.Animation.SlideOutRight);
+                _flipper.ShowPrevious();
+            };
 
         }
     }
 }
+            
+    
+
 
